fdfc696d95834d0aae9cb8ddc009dee2c313b229
Trey Suiter <trey.suiter@gmail.com>
2020-01-13 13:33:29 -0600

    finish practice; still need to do challenge

--- planets.py
+++ planets.py
@@ -1,12 +1,24 @@
-# planet_list = ["Mercury", "Mars"]
+#Initialize list with two planets
+planet_list = ["Mercury", "Mars"]
 
-# planet_list.append("Jupiter")
-# planet_list.append("Saturn")
-# last_two_planets = ["Uranus", "Neptune"]
-# planet_list.extend(last_two_planets)
-# planet_list.insert(2, "Venus")
+#Add jupiter and Saturn
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
 
-my_list = [1, 2, 4, "hello", "Monkey"]
-my_subset = my_list[2:-1]
-print(my_subset)
-print(my_list)
+#Create list of last two planets and add them to planet_list
+last_two_planets = ["Uranus", "Neptune"]
+planet_list.extend(last_two_planets)
+
+#Insert Venus and Earth at popper positions
+planet_list.insert(1, "Venus")
+planet_list.insert(2, "Earth")
+
+#Remove Pluto
+planet_list.append("Pluto")
+
+#Create list of rocky planets
+rocky_planets = planet_list[0:4]
+
+#Delete Pluto
+del planet_list[-1]
+

c73c42eeba55f9576edf88703049e236811efbd8
Trey Suiter <trey.suiter@gmail.com>
2020-01-13 13:25:37 -0600

    added pluto

---
+++ planets.py
@@ -0,0 +1,12 @@
+# planet_list = ["Mercury", "Mars"]
+
+# planet_list.append("Jupiter")
+# planet_list.append("Saturn")
+# last_two_planets = ["Uranus", "Neptune"]
+# planet_list.extend(last_two_planets)
+# planet_list.insert(2, "Venus")
+
+my_list = [1, 2, 4, "hello", "Monkey"]
+my_subset = my_list[2:-1]
+print(my_subset)
+print(my_list)
